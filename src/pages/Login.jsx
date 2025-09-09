14fa83494b2622a9b18703304f5ad79e068dbb7e
unknown <laturuhema1245@gmail.com>
2025-09-09 11:55:52 +0530

    color

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,9 +28,9 @@ export default function Login({ setShowLogin, setShowSignup }) {
             </button>
 
             <div className="flex items-center my-4">
-              <div className="flex-grow border-t border-grey-500"></div>
-              <span className="px-4 text-white text-sm">or login with</span>
               <div className="flex-grow border-t border-gray-500"></div>
+              <span className="px-4 text-white text-sm">or login with</span>
+              <div className="flex-grow border-t border-green-500"></div>
             </div>
 
             <div class="grid grid-cols-3 gap-2 py-2 px-2 text-white">
